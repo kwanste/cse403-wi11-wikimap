02d9a4b0a376c0b367dc70718bf22ca31bb3a4b4
Steven Kwan <Steven Kwan@localhost>
2011-02-06 23:12:36 -0800

    Merging galore.

---
+++ logic/ArticleVector.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ArticleVector {
+	// articleName must be unique
+	public String articleName;
+	
+	// Put relevant data here
+	public List<String> relatedArticles;
+}
---
+++ logic/RelationshipBuilder.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class RelationshipBuilder {
+	public static void build(Map<String, ArticleVector> parsedArticles) {
+		for(ArticleVector article : parsedArticles.values()) {
+			// Process articles
+		}
+	}
+}
