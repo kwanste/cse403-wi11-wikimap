5f581e2fd62f3bc0542e313a78a28dbfdf1d74b8
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-02-07 19:04:49 -0800

    Adding the updateAndCompile.sh script, which downloads the latest update into a folder called wikimap and compiles all Java files inside of it.

--- updateAndCompile.sh
+++ updateAndCompile.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
+rm -r wikimap
 hg clone https://cse403-wi11-wikimap.googlecode.com/hg/ wikimap 
 cd wikimap
-javac *.java
-
-if [ $? -eq 0 ]
-then
-  echo "compile worked!"
-fi
+find . -name "*.java" -print -exec $JAVA_HOME/bin/javac -cp .$CLASSPATH '{} \;
