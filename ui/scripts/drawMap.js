9c0ba2ac0fc664d366af70235c2f19e5eff1d9e3
Liem Dinh <LiemHDinh@gmail.com>
2011-02-12 16:12:39 -0800

    updating the ui graph to be able to hover

--- ui/scripts/drawMap.js
+++ ui/scripts/drawMap.js
@@ -3,8 +3,19 @@ var MAP_WIDTH = 800;
 var ROOT_SIZE = 50;
 var NODE_SIZE = 30;
 var INITIAL_RADIUS = 20;
+var circlesX = [];
+var circlesY = [];
+var circlesTitle = [];
+var canvas;
+var count;
+var CURRENT_ARTICLE = "";
+var HOVER = false;
 
-function drawCircle(ctx, x, y, r) {
+function drawCircle(ctx, x, y, r, title) {
+	circlesX[count] = x;
+	circlesY[count] = y;
+	circlesTitle[count] = title;
+	count++;
 	ctx.beginPath();
 	ctx.strokeStyle = '#AAAAAA';
 	ctx.fillStyle = '#CDCECE';
@@ -30,6 +41,8 @@ function writeText(ctx, text, x, y){
 }
 
 function drawMapHelper(ctx, string, pipe, radius, startAngle, angleSize){
+	var x;
+	var y;
 	console.log("running on: " + string + ", with pipe: " + pipe);
 	if(radius >= MAP_WIDTH / 2 || radius >= MAP_HEIGHT / 2){
 		console.log("out of range");
@@ -37,7 +50,10 @@ function drawMapHelper(ctx, string, pipe, radius, startAngle, angleSize){
 	}else if(pipe == ""){
 		console.log("drawing base case");
 		var angle = startAngle + angleSize / 2;
-		drawCircle(ctx, MAP_WIDTH / 2 + radius * Math.cos(angle), MAP_HEIGHT / 2 + radius * Math.sin(angle), NODE_SIZE);
+		x = MAP_WIDTH / 2 + radius * Math.cos(angle);
+		y = MAP_HEIGHT / 2 + radius * Math.sin(angle);
+		drawCircle(ctx, x, y, NODE_SIZE, string);
+		writeText(ctx, string, x - 22, y - 10); 
 	}else{
 		var items = string.split(pipe);
 		console.log("iterating helper method");
@@ -50,19 +66,27 @@ function drawMapHelper(ctx, string, pipe, radius, startAngle, angleSize){
 // FORMAT
 // 	PARENT//Child1|Child2|Child3//Child1a|Child1b||Child2a|Child2b||Child3a|Child3b//
 function drawMap(treeString){
-	var canvas = document.getElementById('mapView');
+	canvas = document.getElementById('mapView');
 	// Make sure we don't execute when canvas isn't supported
 	if (canvas.getContext){
 		console.log("start");
+		circlesX = [];
+		circlesY = [];
+		circlesTitle = [];
+		count = 0;
 
 		// use getContext to use the canvas for drawing
 		var ctx = canvas.getContext('2d');
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		ctx.beginPath();
 		var depthSplit = treeString.split("//");
 		var depths = depthSplit.length;
 		var levelPipes = "";
 
 		// draw parent
+		CURRENT_ARTICLE = depthSplit[0];
 		drawCircle(ctx, MAP_WIDTH / 2, MAP_HEIGHT / 2, ROOT_SIZE);
+		writeText(ctx, CURRENT_ARTICLE, MAP_WIDTH / 2 - 30, MAP_HEIGHT / 2 - 10);
 
 		for (var i = 1; i < depths; i++){
 			levelPipes = levelPipes.concat("|");
@@ -74,36 +98,85 @@ function drawMap(treeString){
 	}
 }
 
-function drawShape(){
-	// get the canvas element using the DOM
-	var canvas = document.getElementById('mapView');
-	// Make sure we don't execute when canvas isn't supported
-	if (canvas.getContext){
-
-		// use getContext to use the canvas for drawing
-		var ctx = canvas.getContext('2d');
-
-		// Draw shapes
-		drawCircle(ctx, 400, 300, 50);
-		drawCircle(ctx, 300, 200, 30);
-		drawCircle(ctx, 500, 200, 30);
-		drawCircle(ctx, 500, 400, 30);
-		drawCircle(ctx, 300, 400, 30);
-
-		drawCircle(ctx, 400 + 141.42, 300, 30);
-		drawCircle(ctx, 400 - 141.42, 300, 30);
-		drawCircle(ctx, 400, 300 + 141.42, 30);
-		drawCircle(ctx, 400, 300 - 141.42, 30);
-
-		drawLine(ctx, 400 + 50, 300, 541.42 - 30, 300);
-		drawLine(ctx, 400 - 50, 300, 400 - 141.42 + 30, 300);
-		drawLine(ctx, 400, 300 - 50, 400, 300 - 141.42 + 30);
-		drawLine(ctx, 400, 300 + 50, 400, 300 + 141.42 - 30);
-		
-		writeText(ctx, searchString, 400 - 30, 300 - 7.5);
 
 
-	} else {
-		alert('You need Safari or Firefox 1.5+ to see this Map.');
+function clickedMouse(cx, cy) {
+	for (var i = 1; i < circlesX.length - 1; i++) {
+		if (intersects(circlesX[i], circlesY[i], cx, cy, 30)) {
+			//alert(circlesTitle[i]);
+			console.log(circlesTitle[i]);
+			getPreviewText(circlesTitle[i]);
+			getImageURL(circlesTitle[i]);
+			getArticlePage(circlesTitle[i]);
+			drawMap("Amazon.com//Bill Gates|Child2|Child3|Child4|Child5|Child6"
+	 + "//Child1a|Child1b||Child2a|Child2b||Child3a|Child3b||Child4a|Child4b||Child5a|Child5b||Child6a|Child6b");
+		}
 	}
 }
+
+function mouseMove(cx, cy) {
+	var oldHover = HOVER;
+	var currentlyHover = false;
+	for (var i = 1; i < circlesX.length - 1; i++) {
+		if (intersects(circlesX[i], circlesY[i], cx, cy, 30)) {
+			currentlyHover = true;
+			if (!HOVER) {
+				console.log(circlesTitle[i]);
+				getPreviewText(circlesTitle[i]);
+				getImageURL(circlesTitle[i]);
+				HOVER = true;
+			}
+		}
+	}
+	if (!currentlyHover && HOVER) {
+		HOVER = false;
+		console.log(CURRENT_ARTICLE);
+		getPreviewText(CURRENT_ARTICLE);
+		getImageURL(CURRENT_ARTICLE);
+	}
+}
+
+function intersects(x, y, cx, cy, r) {
+    dx = x-cx
+    dy = y-cy
+    return dx*dx+dy*dy <= r*r
+}
+
+
+function mapInit() {
+	count = 0;
+	canvas = document.getElementById('mapView');
+	canvas.addEventListener("click", 
+						function(e) { 
+							var x;
+							var y;
+							if (e.pageX || e.pageY) { 
+							  x = e.pageX;
+							  y = e.pageY;
+							}
+							else { 
+							  x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+							  y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+							} 
+							x -= canvas.offsetLeft;
+							y -= canvas.offsetTop;
+							clickedMouse(x, y);
+						}, false);
+	canvas.addEventListener("mousemove", 
+						function(e) { 
+							var x;
+							var y;
+							if (e.pageX || e.pageY) { 
+							  x = e.pageX;
+							  y = e.pageY;
+							}
+							else { 
+							  x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+							  y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+							} 
+							x -= canvas.offsetLeft;
+							y -= canvas.offsetTop;
+							mouseMove(x, y);
+						}, false);
+}
+
