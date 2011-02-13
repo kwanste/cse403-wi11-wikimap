6ceb128c6a9f8f868b6f51de2d5915ff7ff43c0d
kshayk <kshayk@localhost>
2011-02-12 23:11:49 -0800

    Test plan and multiple test documents with JUnit test outlines.

---
+++ README.txt
@@ -0,0 +1,5 @@
+Sign off on if you have this file and push it back up to the Google Code Repo
+
+-Steven Kwan
+-Michael Amorozo
+-Kimberly Koenig
---
+++ ui/scripts/wikiSearch.js
@@ -0,0 +1,93 @@
+var url = window.location.href;
+var urlBroken = url.split('?');
+var findSearch = urlBroken[1].split('=');
+var searchString = findSearch[1];
+searchString = searchString.replace("%20", " ");
+
+var jQuery = window.jQuery = window.$ = function(selector, context)
+    {
+       // ...
+       // other internal initialization code goes here
+    };
+
+function fileNotFound(search) {
+	$.ajax({
+	   type: "POST",
+	   async: true,
+	   url: "scripts/dummySearchResults.php",
+	   data: "s=" + search,
+	   success: function(responseText){
+			$('#mapView').css('display', 'none');
+			$('#articleView').css('display', 'block');
+			$('#articleView').html(responseText);
+	   }
+	 });
+}	
+	
+function getPreviewText(search){
+	$.ajax({
+	   type: "POST",
+	   async: true,
+	   url: "scripts/retrieverAPI.php",
+	   data: "s=" + search + "&function=getPreviewText",
+	   success: function(responseText){
+			if(responseText != "Not Found")
+				$('#previewText').text(responseText);
+			else 
+				$('#previewText').text("Article Not Found");
+	   }
+	 });
+}
+
+function getImageURL(search){
+	$.ajax({
+	   type: "POST",
+	   async: true,
+	   url: "scripts/retrieverAPI.php",
+	   data: "s=" + search + "&function=getImageURL",
+	   success: function(responseText){
+			$('#thumbnailImage').attr("src", responseText);
+			$('#thumbnailImage').css("display", "block");
+			if (responseText == "images/image_not_found.png") {
+				$('#thumbnailImage').css("width", "195px");
+				$('#thumbnailImage').css("height", "200px");
+			}
+	   }
+	 });
+}
+
+function getArticlePage(search) {
+	// We should be getting an article for the current page
+	// right now it just grabs the summary and makes the current page that. 
+	// But we should really be getting the page from wikipedia and processing it
+	$.ajax({
+	   type: "POST",
+	   async: true,
+	   url: "scripts/retrieverAPI.php",
+	   data: "s=" + search + "&function=getPreviewText",
+	   success: function(responseText){
+			if(responseText == "Not Found")
+				fileNotFound(search);
+			else 
+				$('#articleView').text(responseText);
+	   }
+	 });
+}
+
+function toggleMap() {
+	if ($('#mapView').css('display') == 'none')
+	{
+		$('#mapView').css('display', 'block');
+		$('#articleView').css('display', 'none');
+	} else {
+		$('#mapView').css('display', 'none');
+		$('#articleView').css('display', 'block');
+	}
+}
+
+function initialize() {
+	getPreviewText(searchString);
+	getImageURL(searchString);
+	getArticlePage(searchString);
+	drawShape();
+}
