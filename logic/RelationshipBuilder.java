fde1d23906157e90a250c37db19d7accaa9117dc
robotoer <robotoer@localhost>
2011-02-14 02:37:56 -0800

    properly handled redirects (still may contain problems with link names

--- logic/RelationshipBuilder.java
+++ logic/RelationshipBuilder.java
@@ -6,31 +6,25 @@ import communication.*;
 public class RelationshipBuilder {
 	public static void build(Map<String, ArticleVector> parsedArticles) {
 		for(ArticleVector article : parsedArticles.values()) {
-			System.out.println("-->>Parsing: " + article.articleName);
+			System.out.println("Parsing: " + article.articleName);
 			// Process articles:
 			
 			// Use links as relations (for now)
 			int strength = 0;
 			Map<String, Integer> relations = new HashMap<String, Integer>();
 			for(String rel_article : article.links) {
-				ArticleVector current = parsedArticles.get(rel_article);
-				if(current != null) {
-					
-					if(current.redirect) {
-						System.out.println("Found a redirect: " + rel_article);
-						relations.put(rel_article, -1);
-					} else {
-						System.out.println("Normal relationship: " + rel_article);
-						relations.put(rel_article, strength);
-						strength++;
-					}
-					if(strength > 20) {
-						System.out.println("Strength greater than 20.");
-						break;
-					}
-				} else {
-					System.out.println("skipping null related article: " + rel_article);
-				}
+			    if(article.redirect) {
+				System.out.println("\tFound a redirect: " + rel_article);
+				relations.put(rel_article, -1);
+			    } else {
+				System.out.println("\tNormal relationship: " + rel_article);
+				relations.put(rel_article, strength);
+				strength++;
+			    }
+			    if(strength > 20) {
+				System.out.println("\tStrength greater than 20.");
+				break;
+			    }
 			}
 			try {
 			    DatabaseUpdater.updateRelevantNodes(article.articleName, relations);
