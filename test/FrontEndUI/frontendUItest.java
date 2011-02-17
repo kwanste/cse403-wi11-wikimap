393d30408c665615bcaec11aed4700518a0a38fc
killea <killea@localhost>
2011-02-17 10:32:56 -0800

    reorganized frontendUI

--- test/FrontEndUI/frontendUItest.java
+++ test/FrontEndUI/frontendUItest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.server.SeleniumServer;
 
-public class FrontendUITest extends SeleneseTestCase {
+public class frontendUItest extends SeleneseTestCase {
 	
 	private SeleniumServer server;
 		
