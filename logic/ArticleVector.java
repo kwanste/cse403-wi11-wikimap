36d66b8e725bd8b3494d9a8252f0bd5020ade18d
Michael <mikerozo88%40gmail.com@localhost>
2011-02-19 17:41:54 -0800

    added singleton and encapsulation to wikiparser

--- logic/ArticleVector.java
+++ logic/ArticleVector.java
@@ -3,11 +3,39 @@ package logic;
 import java.util.*;
 
 public class ArticleVector {
-	// articleName must be unique
-	public String articleName;
-	
-	// Put relevant data here
-	public List<String> links;
-	
-	public boolean redirect;
+    // articleName must be unique
+    private String articleName;
+    // Put relevant data here
+    private List<String> links;
+    private boolean redirect;
+    
+    public ArticleVector(){
+	articleName = null;
+	links = null;
+	redirect = false;
+    }
+
+    public String getArticleName(){
+	return articleName;
+    }
+
+    public List<String> getLinks(){
+	return links;
+    }
+
+    public boolean getRedirect(){
+	return redirect;
+    }
+
+    public void setArticleName(String name){
+	articleName = name;
+    }
+
+    public void setLinks(List<String> list){
+	links = list;
+    }
+
+    public void setRedirect(boolean redirect){
+	this.redirect = redirect;
+    }
 }
