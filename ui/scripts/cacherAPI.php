ba718e67ccf68b592c36326a901a8b7a2432fa9c
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-02-20 14:52:41 -0800

    Added swap.png arrows to the UI images folder.

---
+++ ui/scripts/cacherAPI.php
@@ -0,0 +1,23 @@
+<?php
+	
+	include("cacher.php");
+	$db_cache = new DatabaseCacher;
+	$article = $_POST['article'];
+	$data = $_POST['data'];
+	$function = $_POST['function'];
+	
+	/* For Debugging
+	$article = $_GET['article'];
+	$data = $_GET['data'];
+	$function = $_GET['function'];
+	*/
+  
+	// next line is a temporary hack just for the alpha
+	//$foundarticle = $db_ret->getPreviewText($article) != null
+	if ($function == 'insertImageURL') {
+		$db_cache->insertImageURL($article, $data);
+	}
+	else if ($function == 'insertPreviewText') {
+		$db_cache->insertPreviewText($article, $data);
+	}
+?>
