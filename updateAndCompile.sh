fc79e8151522be3c595fe70d1d7f75a1ce5e5e32
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-02-22 21:54:50 -0800

    Removed files that were moved to other folders.

--- updateAndCompile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -r wikimap
-hg clone https://cse403-wi11-wikimap.googlecode.com/hg/ wikimap 
-cd wikimap
-find . -name "*.java" -print -exec $JAVA_HOME/bin/javac -cp .:$CLASSPATH '{}' \;
