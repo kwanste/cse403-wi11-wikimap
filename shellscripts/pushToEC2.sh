3ae2a8787e1a8677ed3d81ef950c791abcd06d78
Liem Dinh <LiemHDinh@gmail.com>
2011-02-23 11:57:43 -0800

    added a new script file to push to EC2

--- shellscripts/pushToEC2.sh
+++ shellscripts/pushToEC2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 hg clone https://cse403-wi11-wikimap.googlecode.com/hg/ cse403-wi11-wikimap 
-scp -r cse403-wi11-wikimap/ui/* iprojsrv.cs.washington.edu:../../inetpub/wwwroot/.
+scp -i cse403.pem -r cse403-wi11-wikimap/ui/* bitnami@184.72.224.72:../../var/www/.
 rm -r cse403-wi11-wikimap
