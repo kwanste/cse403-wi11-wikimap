be558a9131fd34bec4e0138f3c0c687fe5a9b7cf
Steven Kwan <kwanste@gmail.com>
2011-02-28 21:44:27 -0800

    Added three new tests for updating the db contents in the RelatedArticle table, ArticleSummary table, and ArticleImageURL table. There is redundant code and refactoring that needs to occur.

---
+++ communication/DatabaseConnection.java
@@ -0,0 +1,56 @@
+package communication;
+
+/*
+ * Connection class.  Has one function that returns the current database
+ * credentials including database to connect to in a single Connection 
+ * object. A default is set, but can be overridden.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+	// Change to cse403.cdvko2p8yz0c.us-east-1.rds.amazonaws.com after BETA.
+	private final static String DEFAULT_SRV = "localhost";
+	private final static String DEFAULT_USER = "wikiwrite";
+	private final static String DEFAULT_PASS = "WikipediaMaps123";
+	private final static String DEFAULT_DB = "wikimapsdb_test";
+
+	// Custom Connection
+	public static Connection getConnection(
+			String userServer, 
+			String userUsername, 
+			String userPassword, 
+			String userDB) 
+	{
+		Connection _con = null;
+		
+		try 
+		{
+			if(_con == null || _con.isClosed())
+			{
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+				String server = userServer;
+				String user = userUsername;
+				String pass = userPassword;
+				String db = userDB;
+
+				String url = "jdbc:mysql://" + server + "/" + db;
+				_con = DriverManager.getConnection(url, user, pass);
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return _con;
+	}
+
+	// Create a default connection if no parameters are passed through.
+	public static Connection getConnection() {
+		return getConnection(DEFAULT_SRV, DEFAULT_USER, DEFAULT_PASS, DEFAULT_DB);
+	}
+
+}
---
+++ ui/scripts/searchSuggestions.php
@@ -0,0 +1,45 @@
+<?php
+	$debug = false;
+	if ($debug)
+		$search = $_GET['s'];
+	else
+		$search = $_POST['s'];
+	echo getSearchSuggestions($search);
+	
+	function getSearchSuggestions($search)
+	{
+		$appID = "CAA056181AE100040438EF456936CE6E1763E75A";
+		$query = "http://api.search.live.net/json.aspx?Appid=".$appID."&query="
+			.$search."+site%3Aen.wikipedia.org&sources=web+spell";
+		$result = file_get_contents($query);
+		$parsed = json_decode($result);
+		$returnText = "Article Not Found";
+		if(isset($parsed->SearchResponse->Spell))
+		{
+			$suggestion = $parsed->SearchResponse->Spell->Results[0]->Value;
+			$returnText .= "<br><br>Did you mean: ";
+			$suggestion = str_replace(" site:en.wikipedia.org", "", $suggestion);
+			$returnText .= GenerateLink($suggestion);
+			$search = $suggestion;
+		}
+		if(isset($parsed->SearchResponse->Web->Results))
+		{
+			$searchResults = $parsed->SearchResponse->Web->Results;
+			foreach($searchResults as $value)
+			{
+				$articleTitle = str_replace(" - Wikipedia, the free encyclopedia", "", $value->Title);
+				$returnText .= "<br><br>";	
+				$returnText .= GenerateLink($articleTitle);
+				$description = str_ireplace($search, "<b>".$search."</b>", $value->Description);
+				$returnText .= "<br>".$description;						
+			}
+		}
+		return $returnText;
+	}		
+	
+	function GenerateLink($article)
+	{
+		return "<a href = \"wikiSearch.php?s=".$article."\">"
+			.$article."</a>"; 
+	}		
+?>
