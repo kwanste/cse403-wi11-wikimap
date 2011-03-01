18c8b3909ba5e2d9582ed940db83c777d6f79271
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-03-01 12:48:59 -0800

    Fixed issue 110 (malicious script insertion) by adding htmlspacialchars to searchbar.php.

--- ui/searchbar.php
+++ ui/searchbar.php
@@ -10,10 +10,9 @@
 </script>
 
 <form id="searchForm" method="post" action="wikiSearch.php">
-	<?php echo '<input id="search" name="search" type="search" value="'.$_GET['s'].'" size="20">'; ?>
+	<?php echo '<input id="search" name="search" type="search" value="'.htmlspecialchars($_GET['s']).'" size="20">'; ?>
 	<select id="language">
 		<option value="en">English</option>
-		<option value="fr">French</option>
 	</select>
 	<input type="submit" value=" ->  " name="go" onClick="javascript:doSubmit();">
 </form>
