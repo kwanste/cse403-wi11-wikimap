c98147117cbd2fc9596bff135ba63fc7f5292407
Michael <mikerozo88%40gmail.com@localhost>
2011-02-28 22:18:47 -0800

    added word count functionality to the parser

---
+++ logic/RelationshipBuilder.java
@@ -0,0 +1,63 @@
+package logic;
+
+import java.sql.*;
+import java.util.*;
+import communication.DatabaseConnection;
+
+public class RelationshipBuilder {
+    private static Connection _con;
+    private static PreparedStatement _occurStatement;
+    private static PreparedStatement _relStatement;
+    private static PreparedStatement _updateStrength;
+
+    public static void main(String[] args) throws SQLException {
+	_con = DatabaseConnection.getConnection("cse403.cdvko2p8yz0c.us-east-1.rds.amazonaws.com", "wikiwrite", "WikipediaMaps123", "wikimapsdb_final");
+
+	_occurStatement = _con.prepareStatement("SELECT * FROM WordCounts WHERE Article = ?");
+	_relStatement = _con.prepareStatement("SELECT * FROM ArticleRelations");
+	_updateStrength = _con.prepareStatement("UPDATE ArticleRelations SET Strength = ? WHERE Article = ? AND RelatedArticle = ?");
+	
+	// Iterate through articlerelations
+	ResultSet relations = _relStatement.executeQuery();
+	while(relations.next()) {
+	    String article = relations.getString("Article"), relatedArticle = relations.getString("RelatedArticle");
+	    _updateStrength.clearParameters();
+	    _updateStrength.setDouble(1, distance(article, relatedArticle));
+	    _updateStrength.setString(2, article);
+	    _updateStrength.setString(3, relatedArticle);
+	}
+    }
+
+    private static double distance(String articleName1, String articleName2) throws SQLException {
+	System.out.print("Calculating distance of: (" + articleName1 + ", " + articleName2 + ")... ");
+	Map<String, Integer> article1 = new HashMap<String, Integer>(),
+	    article2 = new HashMap<String, Integer>();
+	double result = 0.0;
+
+	// Fill Maps
+	_occurStatement.clearParameters();
+	_occurStatement.setString(1, articleName1);
+	mapFromResult(_occurStatement.executeQuery(), article1);
+	_occurStatement.clearParameters();
+	_occurStatement.setString(1, articleName2);
+	mapFromResult(_occurStatement.executeQuery(), article2);
+
+	// Generate Word Set
+	Set<String> language = new HashSet<String>();
+	language.addAll(article1.keySet());
+	language.addAll(article2.keySet());
+
+	// calculate 
+	for(String word : language)
+	    result += Math.pow(Math.log(article1.get(word) - article2.get(word)), 2);
+
+	System.out.println(result);
+	return result;
+    }
+
+    private static void mapFromResult(ResultSet result, Map<String, Integer> outMap) throws SQLException {
+	while(result.next()) {
+	    outMap.put(result.getString("Word"), result.getInt("Occurrences"));
+	}
+    }
+}
---
+++ ui/searchbar.php
@@ -0,0 +1,19 @@
+<!-- This file contains the searchBar functionality for the WikiMap UI. -->
+
+<script type="text/javascript">
+	function doSubmit()
+	{
+		var theForm=document.getElementById("searchForm");
+		theForm.action = 'wikiSearch.php?s=' + document.getElementById("search").value;
+		theForm.submit();
+	}
+</script>
+
+<form id="searchForm" method="post" action="wikiSearch.php">
+	<?php echo '<input id="search" name="search" type="search" value="'.$_GET['s'].'" size="20">'; ?>
+	<select id="language">
+		<option value="en">English</option>
+		<option value="fr">French</option>
+	</select>
+	<input type="submit" value=" ->  " name="go" onClick="javascript:doSubmit();">
+</form>
