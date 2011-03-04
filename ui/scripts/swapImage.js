f8667caf455fd7c6e916255a4d9306ee848b8293
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-03-03 21:37:34 -0800

    Added swapImage.js, modified wikiSearch.php, and added a "view map" image.

---
+++ ui/scripts/swapImage.js
@@ -0,0 +1,14 @@
+var intImage = 2;
+
+function swapImage() {
+switch (intImage) {
+ case 1:
+   IMG1.src = "images/view_article_logo_small.png";
+   intImage = 2;
+   return(false);
+case 2:
+   IMG1.src = "images/view_map_small.png";
+   intImage = 1;
+   return(false);
+ }
+}
