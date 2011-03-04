a3f4a474125d439759ff1462472a0391d4de7ade
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-03-03 23:25:19 -0800

    Updated search_button image.

---
+++ ui/scripts/cacherAPI.php
@@ -0,0 +1,36 @@
+<?php
+	// This API is used by the front-end to store image URLs and preview texts of articles that
+	// are retrieved later. It is also used by the database retriever to cache relevancy trees and
+	// for removing expired relevancy trees.
+	include("cacher.php");
+	$db_cache = new DatabaseCacher;
+	$debug = false;
+
+	if($debug) {
+			$function = $_GET['function'];
+			$article = $_GET['article'];
+			$maxDepth = $_GET['maxDepth'];
+			$data = $_GET['data'];
+	} else {
+			$function = $_POST['function'];;
+			$article = $_POST['article'];
+			$maxDepth = $_POST['maxDepth'];
+			$data = $_POST['data'];
+	}
+
+	// next line is a temporary hack just for the alpha
+	//$foundarticle = $db_ret->getPreviewText($article) != null
+	if ($function == 'insertImageURL') {
+			$db_cache->insertImageURL($article, $data);
+	}
+	else if ($function == 'insertPreviewText') {
+			$db_cache->insertPreviewText($article, $data);
+	}
+	else if ($function == 'insertTree'){
+			$db_cache->insertTree($article, $maxDepth, $data);
+	}
+	else if ($function == 'refreshCache'){
+			$db_cache->refreshCache();
+	}
+
+?>
