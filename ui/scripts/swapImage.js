1ca11a2004b2f01af0b992c0dff734339b445bfa
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-03-04 13:38:12 -0800

    Removed unnecessary .pngs and replaced with .jpgs. Updated files to reflect use of .jpgs. Fixed CSS  to have white BG instead of grey.

--- ui/scripts/swapImage.js
+++ ui/scripts/swapImage.js
@@ -1,13 +1,18 @@
+/* This JavaScript file handles the swapping the "View Full Article" and "View Full Map" images
+*  in the WikiMap sidebar.  It is modified from a script found at DevX (http://www.devx.com/tips/Tip/13653)
+*  to ensure FireFox compatibility.
+*/
+
 var intImage = 2;
 
 function swapImage(IMG1) {
 switch (intImage) {
  case 1:
-   IMG1.src = "images/view_full_article.png";
+   IMG1.src = "images/view_full_article.jpg";
    intImage = 2;
    return(false);
 case 2:
-   IMG1.src = "images/view_full_map.png";
+   IMG1.src = "images/view_full_map.jpg";
    intImage = 1;
    return(false);
 default: 
--- ui/wikiSearch.php
+++ ui/wikiSearch.php
@@ -51,7 +51,7 @@ SideBar, Thumbnail, etc. -->
 				<div id="swap">
 					<p align="center" class="swap">
 						<a href = "javascript:toggleMap();" >
-							<img id="IMG1" name="IMG1" onclick="swapImage(IMG1);" src="images/view_full_article.png" style="vertical-align: middle" 
+							<img id="IMG1" name="IMG1" onclick="swapImage(IMG1);" src="images/view_full_article.jpg" style="vertical-align: middle" 
 							alt="Click to view full article" border="0" /></a></p>
 				</div>
 				<b>
