3e958fb28b36dab1068fec23677fe683900c2cd8
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-03-06 20:49:14 -0800

    Added the main UI code and CSS for the logo.

--- ui/index.php
+++ ui/index.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<title>WikiMaps</title>
+   <head>
+   <meta charset="utf-8" />
+   <title>WikiMaps</title>
 
-		<link rel="stylesheet" href="css/main.css" type="text/css" />
+   <link rel="stylesheet" href="css/main.css" type="text/css" />
 
-		<!--[if IE]>
-			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-		<!--[if lte IE 7]>
-			<script src="js/IE8.js" type="text/javascript"></script><![endif]-->
-		<!--[if lt IE 7]>
-			<link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
-	</head>
+   <!--[if IE]>
+   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+   <!--[if lte IE 7]>
+   <script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+   <!--[if lt IE 7]>
+   <link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
+   </head>
 
-	<body id="index" class="home">
-		
-		<div id="searchRow">
-			<div id="searchColumn">
-				<p id="title">Wiki<b>Map<b></p>
-				<?php include("searchbar.php") ?>
-			</div>
-		</div>
-		
-	</body>
+<body id="index" class="home">
+<div id="searchRow">
+   <!-- Save for Web Slices -->
+<table id="center" width="551" height="541" border="0" cellpadding="0" cellspacing="0">
+   <tr>
+   <td>
+   <img src="images/mainpage_01.jpg" width="551" height="337" alt=""></td>
+   </tr>
+   <tr>
+   <td>
+  <div id="searchColumn">
+   <?php include("searchbar.php") ?>
+   </div>
+   </tr>
+   <tr>
+   <td>
+   <img src="images/mainpage_03.jpg" width="551" height="140" alt=""></td>
+   </tr>
+</table>
+</div>
+<!-- End Save for Web Slices -->
+   </body>
 </html>
