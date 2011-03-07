9cc68fe6c90d5295602b6e90ecc24a7472d5a61c
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-03-07 15:22:25 -0800

    Fixed main node.png

---
+++ ui/searchbar.php
@@ -0,0 +1,21 @@
+<!-- This file contains the searchBar functionality for the WikiMap UI. -->
+
+<script type="text/javascript">
+	function doSubmit()
+	{
+		var theForm=document.getElementById("searchForm");
+		theForm.action = 'wikiSearch.php?s=' + encodeURI(document.getElementById("search").value);
+		theForm.submit();
+	}
+</script>
+
+<form id="searchForm" method="post" action="wikiSearch.php">
+	<input id="search" name="search" type="search" value="" size="20">
+		<select id="language">
+			<option value="en">English</option>
+		</select>
+	<input type="image" src="images/search_button.png" name="go" onClick="javascript:doSubmit();" style="vertical-align: bottom;">
+	<div id="menu">
+		<a href="about.php">About</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="faq.php>FAQ</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="contact.php">Contact</a>
+	</div>
+</form>
