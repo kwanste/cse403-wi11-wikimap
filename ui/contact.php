eb92d5c366904a0f44b15a4bcb001f80e1bea113
holycraplions%40gmail.com <holycraplions%40gmail.com@localhost>
2011-03-09 21:13:07 -0800

    Making some CSS fixes. Sidebar left padding is slightly larger, and added 1% padding to the top so that "Article Not Found" isn't pushed up against the top of the screen.

--- ui/contact.php
+++ ui/contact.php
@@ -44,7 +44,7 @@ SideBar, Thumbnail, etc. -->
 			</ul>
 			</p>
 			<h1>Contact Us</h1>
-			<p>The WikiMap team may be contacted via email at our <a href="mailto: cse-403-wikimap@googlegroups.com">Google Group</a></a> address.</p>  
+			<p>The WikiMap team may be contacted via email at our <a href="mailto: cse-403-wikimap@googlegroups.com">Google Group</a></a> address.  You may also send bugs to this email address.</p>  
 			<p></p>
 		</div>
 	</div>
