a09c707a45bb3d5e7d11423b5a0ea201a8610fea
horkind <horkind@localhost>
2011-03-10 17:32:55 -0800

    Fixed regression in retriever.php, error for preview map when article not in DB.

---
+++ communication/DatabaseConnection.java
@@ -0,0 +1,58 @@
+package communication;
+
+/*
+ * Connection class.  Has one function that returns the current database
+ * credentials including database to connect to in a single Connection 
+ * object. A default is set, but can be overridden.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+	
+	private final static String DEFAULT_SRV = "cse403.cdvko2p8yz0c.us-east-1.rds.amazonaws.com";
+	private final static String DEFAULT_USER = "wikiwrite";
+	private final static String DEFAULT_PASS = "WikipediaMaps123";
+	private final static String DEFAULT_DB = "wikimapsDB_final";
+
+	// Custom Connection
+        // Use this for development.
+	public static Connection getConnection(
+			String userServer, 
+			String userUsername, 
+			String userPassword, 
+			String userDB) 
+	{
+		Connection _con = null;
+		
+		try 
+		{
+			if(_con == null || _con.isClosed())
+			{
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+				String server = userServer;
+				String user = userUsername;
+				String pass = userPassword;
+				String db = userDB;
+
+				String url = "jdbc:mysql://" + server + "/" + db;
+				_con = DriverManager.getConnection(url, user, pass);
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return _con;
+	}
+
+	// Create a default connection if no parameters are passed through.
+        // All released classes should be using this in the live environment
+	public static Connection getConnection() {
+		return getConnection(DEFAULT_SRV, DEFAULT_USER, DEFAULT_PASS, DEFAULT_DB);
+	}
+
+}
